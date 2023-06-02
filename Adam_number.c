33991120a2e716636d31ed890de59d5967b75508
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2023-06-02 16:08:00 +0530

    #include<stdio.h> int main() {     int a,b,r,q,sq,ev=0,k,rev=0;     scanf("%d",&a);     q=a*a;     while(a!=0){         r=a%10;         rev=rev*10+r;         a=a/10;
        }
        sq=rev*rev;
        while(sq!=0){
            k=sq%10;
            ev=ev*10+k;
            sq=sq/10;
            
        }
        if(ev==q) printf("True");
        else printf("False");}

--- Adam_number.c
+++ Adam_number.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,sq,i,remainder,reverse=0,sq2,remainder2,reverse2=0;
-    scanf("%d",&n);
-    sq=n*n;
-    while (n>0) {
-        remainder=n%10;
-        reverse=reverse*10+remainder;
-        n/=10;
+    int a,b,r,q,sq,ev=0,k,rev=0;
+    scanf("%d",&a);
+    q=a*a;
+    while(a!=0){
+        r=a%10;
+        rev=rev*10+r;
+        a=a/10;
+        
     }
-  sq2=reverse*reverse;
-  while (sq2>0) {
-        remainder2=sq2%10;
-        reverse2=reverse2*10+remainder2;
-        sq2/=10;
+    sq=rev*rev;
+    while(sq!=0){
+        k=sq%10;
+        ev=ev*10+k;
+        sq=sq/10;
+        
     }
-    if(sq==reverse2) {
-        printf("True");
-    }
-    else {
-        printf("False");
-    }
-}
+    if(ev==q) printf("True");
+    else printf("False");}
